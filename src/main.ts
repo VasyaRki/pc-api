6c286f48f657e141577cf39fd9e6f50e9151ada0
synko6 <vasil.synko@gmail.com>
2023-10-06 13:48:51 +0300

    Setting cors

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,9 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({ credentials: true, origin: '*' });
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(300);
 }
 bootstrap();

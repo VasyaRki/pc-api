9f2f9f7cb3f1bdae6da626ff9eba32e8fd2513f7
synko6 <vasil.synko@gmail.com>
2023-10-09 20:44:05 +0300

    Remove all.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,20 @@
-import { JwtModule } from '@nestjs/jwt';
+// import { JwtModule } from '@nestjs/jwt';
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+// import { ConfigModule } from '@nestjs/config';
 
-import { DbModule } from './db/db.module';
+// import { DbModule } from './db/db.module';
 import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
-import { UserModule } from './user/user.module';
+// import { AuthModule } from './auth/auth.module';
+// import { UserModule } from './user/user.module';
 import { AppController } from './app.controller';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    DbModule,
-    JwtModule,
-    AuthModule,
-    UserModule,
+    // ConfigModule.forRoot({ isGlobal: true }),
+    // DbModule,
+    // JwtModule,
+    // AuthModule,
+    // UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
